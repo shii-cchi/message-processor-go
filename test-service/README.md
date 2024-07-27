b9dd7c3fad96e12f8e4cc62a16358cda979b3a6f
Shion Sonozaki <sssyaon@gmail.com>
2024-07-28 05:07:50 +0700

    add test-service

---
+++ test-service/README.md
@@ -0,0 +1,8 @@
+# Test Service for Message Processing
+
+## Описание
+
+Сервис для тестирования микросервиса https://github.com/shii-cchi/message-processor-go, который:
+- принимает сообщения из Кафки, отправленные message-processor-go сервисом
+- "обрабатывает" их
+- отправляет обработанные сообщения в Кафку
---
+++ test-service/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"log"
+	"os"
+)
+
+func main() {
+	log.Println("test-service starting")
+	c, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": "kafka:9092",
+		"group.id":          "myGroup",
+		"auto.offset.reset": "earliest",
+	})
+
+	if err != nil {
+		log.Fatalf("Failed to create consumer: %s\n", err)
+	}
+
+	defer c.Close()
+
+	err = c.Subscribe("to-be-processed", nil)
+	if err != nil {
+		log.Fatalf("Failed to subscribe to topic: %s\n", err)
+	}
+
+	p, err := kafka.NewProducer(&kafka.ConfigMap{
+		"bootstrap.servers": "kafka:9092"})
+
+	if err != nil {
+		fmt.Printf("Failed to create producer: %s\n", err)
+		os.Exit(1)
+	}
+
+	defer p.Close()
+
+	go func() {
+		for e := range p.Events() {
+			switch ev := e.(type) {
+			case *kafka.Message:
+				if ev.TopicPartition.Error != nil {
+					log.Printf("Delivery failed: %v\n", ev.TopicPartition)
+				} else {
+					log.Printf("Delivered message to %v\n", ev.TopicPartition)
+				}
+			}
+		}
+	}()
+
+	msgChan := make(chan *kafka.Message)
+
+	go func() {
+		for {
+			msg, err := c.ReadMessage(-1)
+			if err == nil {
+				log.Printf("Received message: %s\n", string(msg.Value))
+			} else {
+				log.Printf("Consumer error: %v (%v)\n", err, msg)
+			}
+
+			msgChan <- msg
+		}
+	}()
+
+	topic := "processed-messages"
+	for msg := range msgChan {
+		err := p.Produce(&kafka.Message{
+			TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+			Value:          msg.Value,
+		}, nil)
+
+		if err != nil {
+			log.Printf("Failed to produce message: %s\n", err)
+		}
+	}
+
+	p.Flush(15 * 1000)
+}
